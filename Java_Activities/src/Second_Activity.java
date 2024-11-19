61ee36be462c38bf17a7a987344093015590d5ff
Cris <crisalequintero17@gmail.com>
2024-11-19 12:30:25 -0600

    Add files via upload

---
+++ Java_Activities/src/Second_Activity.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+public class Second_Activity {
+   public static void main (String[]args){
+       byte x,y;
+       long z;
+       System.out.println("Ingresa dos valores");
+       x=Byte.parseByte(JOptionPane.showInputDialog("Ingresa el primer valor"));
+        y=Byte.parseByte(JOptionPane.showInputDialog("Ingresa el segundo valor"));
+        z=x+y;
+      System.out.println("Es resultado de la suma es : "+ z);  
+   } 
+}

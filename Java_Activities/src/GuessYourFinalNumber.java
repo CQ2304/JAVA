4790c33d3d74569b0c110f8950145caaa10ea843
C_R_I_S <crisalequintero17@gmail.com>
2025-01-07 14:29:57 -0600

    Update GuessYourFinalNumber.java

--- Java_Activities/src/GuessYourFinalNumber.java
+++ Java_Activities/src/GuessYourFinalNumber.java
@@ -3,7 +3,7 @@ public class GuessYourFinalNumber {
     public static void main(String[]args){
         byte a=3;
         JOptionPane.showMessageDialog(null,"Bienvenido, a continuacion te solicitare que pienses en un numero");
-         JOptionPane.showMessageDialog(null,"Ahora multiplica tu numero por2");
+         JOptionPane.showMessageDialog(null,"Ahora multiplica tu numero por 2");
          JOptionPane.showMessageDialog(null,"Ahora sumale 9");
          JOptionPane.showMessageDialog(null,"Ahora restale 3");
           JOptionPane.showMessageDialog(null,"Divide tu numero en 2");
